c2f9640fc61dc0128dc092f1e4704c0ad1a8f6cf
Tim BOWMAN <puffy@netherlogic.com>
2012-11-23 18:11:22 -0500

    Add Obstacle object.

--- boids.py
+++ boids.py
@@ -1,10 +1,14 @@
 #
 # Boids in 3D
 #
+# Tim BOWMAN
+# Project home:
+# http://github.com/timbowman/boids
+#
+# Thank you to those that came before me.
 # This code is based on the Boids recipe at this URL:
 # http://code.activestate.com/recipes/502240-boids-version-11/
-#
-# Guided by Conrad Parker's pseudocode.
+# Guided by Conrad Parker's pseudocode:
 # http://www.vergenet.net/~conrad/boids/pseudocode.html
 #
 
@@ -22,6 +26,15 @@ class Vec3D:
 
     def __repr__(self):
         return 'Vec3D(%s, %s, %s)' % (self.x, self.y, self.z)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.x == other.x and \
+                self.y == other.y and \
+                self.z == other.z
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __add__(self, other):
         return Vec3D(self.x + other.x, self.y + other.y, self.z + other.z)
@@ -54,7 +67,17 @@ class Vec3D:
         return self
 
     def mag(self):
+        """Length of vector"""
         return ((self.x ** 2) + (self.y ** 2) + (self.z ** 2)) ** 0.5
+
+    def unit(self):
+        """Unit vector"""
+        return self / self.mag()
+
+    def dist(self, other):
+        """Distance to other"""
+        return (other - self).mag()
+
 
 ###############################################################################
 
@@ -63,13 +86,13 @@ class Vec3D:
 
 class Boid:
     """A boid.
-    
-    clumping_mag -- a scaling factor for rule 1 as suggested by Parker. 
+
+    clumping_mag -- a scaling factor for rule 1 as suggested by Parker.
                         He recommends 1%. (default 0.01)
     min_distance -- how close the boid is comfortable being to other boids.
                         Parker recommends 100 units. (default 100)
-    schooling_mag -- how strongly this boid will conform it's steering to 
-                        those boids around it. Parker recommends 1/8. 
+    schooling_mag -- how strongly this boid will conform it's steering to
+                        those boids around it. Parker recommends 1/8.
                         (default 0.125)
     velocity_max -- should be self-explanatory.
     """
@@ -79,7 +102,7 @@ class Boid:
                  min_distance=100,
                  schooling_mag=0.125,
                  velocity_max=100):
-        # TODO: add radius of "localness", angle of vision, "predatorness", 
+        # TODO: add radius of "localness", angle of vision, "predatorness",
         # "goalness"
         self.position = Vec3D(0, 0, 0)
         self.velocity = Vec3D(0, 0, 0)
@@ -88,11 +111,12 @@ class Boid:
         self.schooling_mag = schooling_mag
         self.velocity_max = velocity_max
         self.boundary = None
+        self.obstacles = []
         self.goal = None
 
     def __repr__(self):
-        return "Boid(%s, %s, %f, %f, %f, %f)" % (self.position, self.velocity, 
-            self.clumping_mag, self.min_distance, self.schooling_mag, 
+        return "Boid(%s, %s, %f, %f, %f, %f)" % (self.position, self.velocity,
+            self.clumping_mag, self.min_distance, self.schooling_mag,
             self.velocity_max)
 
     def set_position(self, px, py, pz):
@@ -111,7 +135,8 @@ class Boid:
         v1 = self.rule1(boids)
         v2 = self.rule2(boids)
         v3 = self.rule3(boids)
-        bound = self.bound_position()
+        bound = self.stay_in_boundary()
+        # TODO: Do obstacle avoidance here.
         self.velocity += v1 + v2 + v3 + bound
         self.limit_speed()
 
@@ -119,7 +144,7 @@ class Boid:
         self.position += self.velocity
 
     def rule1(self, boids):
-        """Rule 1: Boids try to fly towards the centre of mass of neighbouring 
+        """Rule 1: Boids try to fly towards the centre of mass of neighbouring
         boids. A.K.A. "clumping."
         """
         vector = Vec3D(0, 0, 0)
@@ -130,7 +155,7 @@ class Boid:
         return (vector - self.position) * self.clumping_mag
 
     def rule2(self, boids):
-        """Rule 2: Boids try to keep a small distance away from other objects 
+        """Rule 2: Boids try to keep a small distance away from other objects
         (including other boids). A.K.A. "avoidance."
         """
         vector = Vec3D(0, 0, 0)
@@ -141,7 +166,7 @@ class Boid:
         return vector
 
     def rule3(self, boids):
-        """Rule 3: Boids try to match velocity with near boids. A.K.A. 
+        """Rule 3: Boids try to match velocity with near boids. A.K.A.
         "schooling."
         """
         vector = Vec3D(0, 0, 0)
@@ -152,52 +177,61 @@ class Boid:
         return (vector - self.velocity) * self.schooling_mag
 
     def limit_speed(self):
-        """Limiting the speed
-        I find it a good idea to limit the magnitude of the boids' velocities, 
-        this way they don't go too fast. Without such limitations, their speed 
-        will actually fluctuate with a flocking-like tendency, and it is 
-        possible for them to momentarily go very fast. We assume that real 
-        animals can't go arbitrarily fast, and so we limit the boids' speed. 
-        (Note that I am using the physical definitions of velocity and speed 
-        here; velocity is a vector and thus has both magnitude and direction, 
-        whereas speed is a scalar and is equal to the magnitude of the 
-        velocity).
-
-        For a limiting speed vlim:
-
-            PROCEDURE limit_velocity(Boid b)
-                    Integer vlim
-                    Vector v
-
-                    IF |b.velocity| > vlim THEN
-                            b.velocity = (b.velocity / |b.velocity|) * vlim
-                    END IF
-            END PROCEDURE
-
-        This procedure creates a unit vector by dividing b.velocity by its 
-        magnitude, then multiplies this unit vector by vlim. The resulting 
-        velocity vector has the same direction as the original velocity but 
-        with magnitude vlim.
-
-        Note that this procedure operates directly on b.velocity, rather than 
-        returning an offset vector. It is not used like the other rules; 
-        rather, this procedure is called after all the other rules have been 
-        applied and before calculating the new position, ie. within the 
-        procedure move_all_boids_to_new_positions:
-
-                        b.velocity = b.velocity + v1 + v2 + v3 + ...
-                        limit_velocity(b)
-                        b.position = b.position + b.velocity
+        """Limiting the boid's speed
         """
-        
         if self.velocity_max == 0:
             self.velocity *= 0
             return
         if self.velocity.mag() > self.velocity_max:
             self.velocity /= self.velocity.mag() / self.velocity_max
 
-    def bound_position(self):
-        """Encourage the boid to stay inside a certain area."""
+    def stay_in_boundary():
+        """Returns the vector to push the boid back insde the boundary if it's
+        outside. Otherwise, returns Vec3D(0, 0, 0).
+        """
+        bound = Vec3D(0, 0, 0)
+        if self.boundary is not None:
+            bound = self.boundary.correction()
+
+    def avoid_obstacles():
+        """TODO: Implement this."""
+        assert False  # TODO: Implement this.
+
+    def tend_to_place(self):
+        """TODO: Tendency towards a particular place
+
+        For example, to steer a sparse flock of sheep or cattle to a narrow
+        gate. Upon reaching this point, the goal for a particular boid could
+        be changed to encourage it to move away to make room for other members
+        of the flock. Note that if this 'gate' is flanked by impenetrable
+        objects as accounted for in Rule 2 above, then the flock will
+        realistically mill around the gate and slowly trickle through it.
+
+        PROCEDURE tend_to_place(Boid b)
+            Vector place
+
+            RETURN (place - b.position) / 100
+        END PROCEDURE
+
+        Note that this rule moves the boid 1% of the way towards the goal at
+        each step. Especially for distant goals, one may want to limit the
+        magnitude of the returned vector.
+        """
+
+        pass
+
+
+class Boundary:
+
+    def __init__(self, min_x = 0, min_y = 0, min_z = 0,
+                       max_x = 100, max_y = 100, max_z = 100,
+                       strength = 10):
+        self.min = Vec3D(min_x, min_y, min_z)
+        self.max = Vec3D(max_x, max_y, max_z)
+        self.strength = strength
+
+    def correction(boid):
+        """Return a Vec3D that will push the boid back inside the boundary."""
 
         v = Vec3D(0, 0, 0)
         if not self.boundary:
@@ -216,75 +250,82 @@ class Boid:
             v.z = -self.boundary.strength
         return v
 
-    def tend_to_place(self):
-        """TODO: Tendency towards a particular place
-        
-        For example, to steer a sparse flock of sheep or cattle to a narrow 
-        gate. Upon reaching this point, the goal for a particular boid could 
-        be changed to encourage it to move away to make room for other members 
-        of the flock. Note that if this 'gate' is flanked by impenetrable 
-        objects as accounted for in Rule 2 above, then the flock will 
-        realistically mill around the gate and slowly trickle through it.
-    
-        PROCEDURE tend_to_place(Boid b)
-            Vector place
-    
-            RETURN (place - b.position) / 100
-        END PROCEDURE
-        
-        Note that this rule moves the boid 1% of the way towards the goal at 
-        each step. Especially for distant goals, one may want to limit the 
-        magnitude of the returned vector.
-        """
-        
-        pass
 
+class Obstacle:
+    """Base class for obstacles.
 
-class Boundary:
+    px, py, pz -- Vec3D position. (default 0, 0, 0)
+    strength -- how much to push the boid away from this obstacle. This is the
+                maximum length for the correction vector.
+    """
 
-    def __init__(self, 
-                 min_x = 0,
-                 min_y = 0,
-                 min_z = 0,
-                 max_x = 100,
-                 max_y = 100,
-                 max_z = 100,
-                 strength = 10):
-        self.min = Vec3D(min_x, min_y, min_z)
-        self.max = Vec3D(max_x, max_y, max_z)
+    def __init__(self, px=0, py=0, pz=0, strength=10):
+        self.position = Vec3D(0, 0, 0)
         self.strength = strength
 
-    def bound_position(boid):
-
-        v = Vec3D()  # vector that will direct boid back inside bounds
-
-        if boid.position.x < min.x:
-            v.x = strength
-        elif boid.position.x > max.x:
-            v.x = -strength
-        if boid.position.y < min.y:
-            v.x = strength
-        elif boid.position.y > max.y:
-            v.x = -strength
-        if boid.position.z < min.z:
-            v.x = strength
-        elif boid.position.z > max.z:
-            v.x = -strength
-
-        return v
-
-
-class Goal:
-
-    def __init__(self, px, py, pz):
+    def set_position(self, px, py, pz):
         self.position = Vec3D(px, py, pz)
 
-
-class Flock:
-    # Collect a group of boids as well as any environmental bits.
-
-    def __init__():
+    def correction(boid):
         pass
+
+
+class ObstacleSphere(Obstacle):
+    """A spherical obstacle.
+
+    px, py, pz -- Vec3D position. (default 0, 0, 0)
+    strength -- how much to push the boid away from this obstacle. This is the
+                maximum length for the correction vector.
+    radius_max -- the correction is at maximum strength at this radius from
+                the center position
+    radius_min --  the correction is at minimum strength (0) at this radius
+                from the center position
+    """
+
+    def __init__(self, px, py, pz, strength, radius_max, radius_min):
+        Obstacle.__init__(self, px, py, pz, strength)
+        self.radius_max = radius_max
+        self.radius_min = radius_min
+
+    def correction(self, boid):
+        """Correction vector for this boid and this obstacle.
+
+        The vector will have a length equal to the obstacle's strength when
+        the boid is inside the radius_max. The vector's length will linearly
+        move to zero as the boid approaches radisu_min. If the boid is not
+        inside radius_min, the vector's length will be zero.
+
+        Returns Vec3D
+        """
+
+        vec_to_boid = boid.position - self.position
+        dist_to_boid = vec_to_boid.mag()
+        #print "vec_to_boid", vec_to_boid, "dist_to_boid", dist_to_boid
+        if dist_to_boid >= self.radius_min:
+            # Return zero vector if boid is farther than radius_min away.
+            return Vec3D(0, 0, 0)
+        elif dist_to_boid <= self.radius_max:
+            # Return the maximum length vector
+            return vec_to_boid.unit() * self.strength
+        else:
+            # Boid is between max and min distance. Interpolate the length of
+            # the vector.
+            mag = (dist_to_boid - self.radius_max) / \
+                    (self.radius_min - self.radius_max)
+            return vec_to_boid.unit() * mag * self.strength
+
+
+#class Goal:
+#
+#    def __init__(self, px, py, pz):
+#        self.position = Vec3D(px, py, pz)
+#
+#
+#class Flock:
+#    # Collect a group of boids as well as any environmental bits.
+#
+#    def __init__():
+#        pass
 
 
 ######################################################
@@ -292,9 +333,9 @@ class Flock:
 #class Boids(list):
 #    """A single flock of boids"""
 #
-#    def __init__(self, boids, randomness=0.01):
-#        for i in range(boids):
-#            self.add_boid()
+#    def __init__(self, boids):
+#        for i in boids:
+#            self.add_boid(i)
 #
 #    def add_boid(self, *args, **kwargs):
 #        pass
